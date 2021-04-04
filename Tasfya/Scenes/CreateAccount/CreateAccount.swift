1ade97582b706aa53f3a1d9bbf03149cd200d67f
Elsaadany <iosbmsc@gmail.com>
2021-04-04 17:54:01 +0200

    start create account screen design

---
+++ Tasfya/Scenes/CreateAccount/CreateAccount.swift
@@ -0,0 +1,17 @@
+//
+//  CreateAccount.swift
+//  Tasfya
+//
+//  Created by Elsaadany on 04/04/2021.
+//
+
+import UIKit
+
+class CreateAccount: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
