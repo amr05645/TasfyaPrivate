a5acc303e5212eb72c809c2bd96f07edf843b575
com.amr <amrhassan702@yahoo.com>
2021-04-05 15:00:06 +0200

    finish account settings design

---
+++ Tasfya/Scenes/Account Settings/AccountSettingsVC.swift
@@ -0,0 +1,31 @@
+//
+//  AccountSettingsVC.swift
+//  Tasfya
+//
+//  Created by Amr on 4/4/21.
+//
+
+import UIKit
+
+class AccountSettingsVC: UIViewController {
+	
+	@IBOutlet weak var usernameTF: UITextField!
+	@IBOutlet weak var phonenumberTF: UITextField!
+	@IBOutlet weak var emailTF: UITextField!
+	@IBOutlet weak var genderTF: UITextField!
+	@IBOutlet weak var countryTF: UITextField!
+	
+	@IBAction func addAdressesBtn(_ sender: Any) {
+	}
+	
+	@IBAction func deleteAccountBtn(_ sender: Any) {
+	}
+	
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
