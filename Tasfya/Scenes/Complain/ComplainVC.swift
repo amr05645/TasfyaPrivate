7bee8aace6146c8ffac028e40a70dc2476fb4a51
Elsaadany <iosbmsc@gmail.com>
2021-04-06 14:31:33 +0200

    finish complain screen design

---
+++ Tasfya/Scenes/Complain/ComplainVC.swift
@@ -0,0 +1,35 @@
+//
+//  ComplainVC.swift
+//  Tasfya
+//
+//  Created by Elsaadany on 06/04/2021.
+//
+
+import UIKit
+
+class ComplainVC: UIViewController {
+    
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var phoneTF: UITextField!
+    @IBOutlet weak var mailTF: UITextField!
+    @IBOutlet weak var messageTV: UITextView!
+    @IBOutlet weak var complainLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        messageTV.delegate = self
+    }
+
+    @IBAction func sendTapped(_ sender: Any) {
+    }
+    
+}
+
+extension ComplainVC: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        print(textView.text.count)
+        complainLabel.isHidden = !(textView.text.isEmpty)
+    }
+}
