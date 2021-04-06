58b9e7606287448c75200be9b152c3b91e367355
Elsaadany <iosbmsc@gmail.com>
2021-04-06 13:15:39 +0200

    finish contact us screen design

---
+++ Tasfya/Scenes/ContactUs/ContactUsVC.swift
@@ -0,0 +1,29 @@
+//
+//  ContactUsVC.swift
+//  Tasfya
+//
+//  Created by Elsaadany on 06/04/2021.
+//
+
+import UIKit
+
+class ContactUsVC: UIViewController {
+    
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var mailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func phoneTapped(_ sender: Any) {
+    }
+    
+    @IBAction func mailTapped(_ sender: Any) {
+    }
+    
+    @IBAction func complainTapped(_ sender: Any) {
+    }
+}
