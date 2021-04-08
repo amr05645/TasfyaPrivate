3dcadd4cb72eeaa4663244d5e3c7a6829da12c33
iosbmsc <iosbmsc@gmail.com>
2021-04-08 11:24:02 +0200

    merge

---
+++ Tasfya/Scenes/NotLoggedIn/NotLoggedVC.swift
@@ -0,0 +1,26 @@
+//
+//  NotLoggedVC.swift
+//  Tasfya
+//
+//  Created by Amr on 4/5/21.
+//
+
+import UIKit
+
+class NotLoggedVC: UIViewController {
+	
+	
+	@IBAction func loginBtn(_ sender: Any) {
+	}
+	
+	@IBAction func createAccountBtn(_ sender: Any) {
+	}
+	
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ Tasfya/Scenes/Verification code/VerifyVC.swift
@@ -0,0 +1,69 @@
+//
+//  VerifyVC.swift
+//  Tasfya
+//
+//  Created by Amr on 4/6/21.
+//
+
+import UIKit
+import KWVerificationCodeView
+
+class VerifyVC: UIViewController{
+	
+	var cowntdownTimer: Timer!
+	var totalTime = 5
+	var btnTitle = "Send again in 50 sec "
+	var attributedTitle = NSMutableAttributedString()
+	
+	
+	@IBOutlet weak var countLabel: UILabel!
+	@IBOutlet weak var verifingTF: KWVerificationCodeView!
+	@IBOutlet weak var sendAgainBtn: UIButton!
+	
+	@IBAction func sendCodeBtn(_ sender: Any) {
+		
+	}
+	
+	@IBAction func loginBtn(_ sender: Any) {
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		verifingTF.textColor = #colorLiteral(red: 0.07058823529, green: 0.1019607843, blue: 0.3137254902, alpha: 1)
+		verifingTF.underlineColor = #colorLiteral(red: 0.07058823529, green: 0.1019607843, blue: 0.3137254902, alpha: 1)
+		startTimer()
+		setAttributedBtn()
+	}
+	
+	
+	func startTimer() {
+		Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {cowntdownTimer in
+			if self.totalTime != 0 {
+				self.sendAgainBtn.isEnabled = false
+				self.totalTime -= 1
+				self.btnTitle = "Send again in \(self.totalTime) sec "
+				self.setAttributedBtn()
+			} else {
+				cowntdownTimer.invalidate()
+				self.sendAgainBtn.isEnabled = true
+			}
+		})
+	}
+	
+	
+	func setAttributedBtn() {
+		btnTitle = "Send again in \(totalTime) sec "
+		attributedTitle = NSMutableAttributedString(string: btnTitle, attributes: [NSAttributedString.Key.font:UIFont(name: "System Font Regular",size: 25)!])
+		
+		attributedTitle.addAttribute(.foregroundColor, value: #colorLiteral(red: 0.07058823529, green: 0.1019607843, blue: 0.3137254902, alpha: 1), range: NSRange(location: 0, length: 13))
+		
+		attributedTitle.addAttribute(.foregroundColor, value: UIColor.red, range: NSRange(location: 14, length: 6))
+		
+		attributedTitle.addAttribute(NSAttributedString.Key.underlineStyle, value: 1, range: NSRange(location: 14, length: 6))
+		
+		countLabel.attributedText = attributedTitle
+	}
+	
+}
+
+
