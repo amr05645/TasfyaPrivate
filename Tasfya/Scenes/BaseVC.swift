480973ace4ccf702427c91076cf777af09e1f28e
iosbmsc <iosbmsc@gmail.com>
2021-04-11 11:17:05 +0200

    add language button

--- Tasfya/Scenes/BaseVC.swift
+++ Tasfya/Scenes/BaseVC.swift
@@ -10,11 +10,13 @@ import UIKit
 class BaseVC: UIViewController {
     
     weak var delegate: SideMenuDelegate?
+    private var languageTF: PickerTF?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setSideMenuBtn()
         self.showLogo()
+        showLanguageBtn()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,6 +35,13 @@ class BaseVC: UIViewController {
         navigationItem.titleView = imageView
     }
     
+    func showLanguageBtn() {
+        let languages = ["En","عربي"]
+        self.languageTF = PickerTF()
+        languageTF?.setInputPickerData(to: languages)
+        setLanguageBtn()
+    }
+    
     private func setSideMenuBtn() {
         let leftBtn = UIButton()
         leftBtn.setImage(#imageLiteral(resourceName: "sideMenuBtn"), for: .normal)
@@ -43,12 +52,24 @@ class BaseVC: UIViewController {
         ])
         
         leftBtn.addTarget(self, action: #selector(sideMenuTapped), for: .touchUpInside)
-        
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftBtn)
+    }
+    
+    private func setLanguageBtn() {
+        languageTF?.text = "عربي"
+        languageTF?.textAlignment = .right
+        languageTF?.textColor = #colorLiteral(red: 0.07100000232, green: 0.1019999981, blue: 0.3140000105, alpha: 1)
+        languageTF?.font = languageTF!.font!.withSize(15)
+
+        languageTF?.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            languageTF!.widthAnchor.constraint(greaterThanOrEqualToConstant: 30)
+        ])
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: languageTF!)
     }
     
     @objc private func sideMenuTapped() {
         delegate?.showSideMenu()
     }
-    
 }
