89ae75bda38289bc08c60a2f174d935a59754517
com.amr <amrhassan702@yahoo.com>
2021-04-11 13:45:49 +0200

    finish product image scene

---
+++ Tasfya/Scenes/Product page/Cells/ProductImgCell.swift
@@ -0,0 +1,20 @@
+//
+//  ProductImgCell.swift
+//  Tasfya
+//
+//  Created by Amr on 4/8/21.
+//
+
+import UIKit
+
+class ProductImgCell: UICollectionViewCell {
+	
+	
+	@IBOutlet weak var productPhoto: UIImageView!
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
