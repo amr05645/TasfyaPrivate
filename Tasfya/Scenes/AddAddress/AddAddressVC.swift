73292ccc7b510cbbd5946687e0bb6055ef64c402
iosbmsc <iosbmsc@gmail.com>
2021-04-14 13:45:38 +0200

    finish addAddress screen design

---
+++ Tasfya/Scenes/AddAddress/AddAddressVC.swift
@@ -0,0 +1,42 @@
+//
+//  AddAddressVC.swift
+//  Tasfya
+//
+//  Created by Elsaadany on 14/04/2021.
+//
+
+import UIKit
+import CoreLocation
+
+class AddAddressVC: UIViewController {
+    
+    private let locationManager = CLLocationManager()
+    var lat: CLLocationDegrees?
+    var lng: CLLocationDegrees?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    @IBAction func locationTapped(_ sender: Any) {
+        locationManager.startUpdatingLocation()
+    }
+    
+    @IBAction func confirmtapped(_ sender: Any) {
+        
+    }
+}
+
+extension AddAddressVC: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return}
+//        mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+        self.lat = location.coordinate.latitude
+        self.lng = location.coordinate.longitude
+                
+        locationManager.stopUpdatingLocation()
+    }
+}
